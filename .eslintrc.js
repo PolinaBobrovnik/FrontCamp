80b5fbcad0cdf30ab314971047e8ff04c4b809a7
Palina Babrounik <Palina_Babrounik@epam.com>
2018-11-21 15:38:19 +0300

    change some project settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,13 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "env": {
-        "browser": true,
-    }
+  "extends": "airbnb-base",
+  "env": {
+    "browser": true,
+  },
+  "rules": {
+    "import/no-extraneous-dependencies": [
+      "error", {
+        "devDependencies": true
+      }
+    ]
+  }
 };
